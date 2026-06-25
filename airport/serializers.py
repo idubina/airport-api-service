98fc6ec78026a781472ff04d75f0f39e6e87c64d
Illia Dubina <armahanz.music@gmail.com>
2026-06-25 21:35:37 +0300

    feat: add model serializers

---
+++ airport/serializers.py
@@ -0,0 +1,133 @@
+from rest_framework import serializers
+from airport.models import (
+    Country,
+    City,
+    Airport,
+    Route,
+    Crew,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Order,
+    TicketClass,
+    Ticket
+)
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = (
+            "id",
+            "name",
+        )
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = (
+            "id",
+            "name",
+            "country",
+        )
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = (
+            "id",
+            "name",
+            "closest_big_city",
+        )
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance"
+        )
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+        )
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = (
+            "id",
+            "name",
+        )
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_rows",
+            "airplane_type",
+            "capacity",
+        )
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+            "base_price",
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "created_at",
+            "user",
+        )
+
+
+class TicketClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TicketClass
+        fields = (
+            "id",
+            "name",
+            "price_multiplier",
+        )
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = (
+            "id",
+            "row",
+            "seat",
+            "flight",
+            "order",
+            "ticket_class",
+            "price"
+        )
