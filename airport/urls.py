835a714e7a2463fd8d0e874c37e370dc04abf932
Illia Dubina <armahanz.music@gmail.com>
2026-06-25 21:42:20 +0300

    feat: add airport API routes
    - add airport app URL configuration
    - register API routes with DRF default router
    - connect airport viewsets to router endpoints
    - include airport API URLs in project urlpatterns

---
+++ airport/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    CountryViewSet,
+    CityViewSet,
+    AirportViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    TicketClassViewSet,
+    TicketViewSet,
+)
+
+app_name = "airport"
+
+router = routers.DefaultRouter()
+
+router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("ticket-classes", TicketClassViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
