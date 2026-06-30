862e14144dbb887a8b7abebedf6d7ad0a68784ae
Illia Dubina <armahanz.music@gmail.com>
2026-06-30 16:59:39 +0300

    chore: configure JWT token lifetime

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -183,4 +183,10 @@ SPECTACULAR_SETTINGS = {
         "defaultModelExpandDepth": 2,
         "defaultModelsExpandDepth": 2,
     },
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True
 }
