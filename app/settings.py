30c851147bb05780060ddb05cff42af041f46206
Illia Dubina <armahanz.music@gmail.com>
2026-06-30 16:08:27 +0300

    style: fiz long string

--- app/settings.py
+++ app/settings.py
@@ -152,7 +152,11 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.LimitOffsetPagination"
+    ),
+    "PAGE_SIZE": 5
 }
 
 MEDIA_ROOT = BASE_DIR / "media"
