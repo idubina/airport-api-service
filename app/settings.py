dd092c037957a49f4f2bb47247ce6eec8a9195d0
Illia Dubina <armahanz.music@gmail.com>
2026-06-30 16:55:22 +0300

    feat: add API throttling

--- app/settings.py
+++ app/settings.py
@@ -159,6 +159,14 @@ REST_FRAMEWORK = {
     ),
     "PAGE_SIZE": 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    }
 }
 
 MEDIA_ROOT = BASE_DIR / "media"
