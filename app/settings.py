ebf117e53107e812f06f919df088237f197ac3a4
Illia Dubina <armahanz.music@gmail.com>
2026-06-30 16:24:12 +0300

    feat: add custom API permissions

--- app/settings.py
+++ app/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "airport.permissions.IsAdminOrAuthenticatedReadOnly",
     ],
     "DEFAULT_PAGINATION_CLASS": (
         "rest_framework.pagination.LimitOffsetPagination"
